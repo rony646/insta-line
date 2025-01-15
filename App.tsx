7eeee36b6b4b812f41de93c41573d82edec5653d
Rony Silva <rony.silva@grupoboticario.com.br>
2025-01-14 21:21:56 -0300

    feat: adding and setting up ui-kitten

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
 
 import Home from "./pages/Home";
 
@@ -8,14 +10,16 @@ const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: #fff;
+  background-color: #e8e8e8;
 `;
 
 export default function App() {
   return (
-    <Container>
-      <Home />
-      <StatusBar style="auto" />
-    </Container>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Container>
+        <Home />
+        <StatusBar style="auto" />
+      </Container>
+    </ApplicationProvider>
   );
 }
